1e341a4aa480f4ec73d26e86d92bc4f25c7336ba
gilushka <EzhiKN@gmail.com>
2022-05-01 11:39:01 +0300

    Оптимизация после занятия про фикстуры
    Добавил в модуль .gitignore потому что без этого не хотела фильтроваться папка build

--- addressbook-web-tests/src/test/java/abTests/ContactCreateTest.java
+++ addressbook-web-tests/src/test/java/abTests/ContactCreateTest.java
@@ -12,26 +12,27 @@ public class ContactCreateTest {
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-//    System.setProperty("webdriver.chrome.driver", "C:\Windows\System32\chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    js = (JavascriptExecutor) wd;
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testContactCreate() throws Exception {
-    wd.get("http://localhost/addressbook/");
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
     wd.findElement(By.linkText("add new")).click();
+    wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
     wd.findElement(By.name("firstname")).sendKeys("Вован");
+    wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
     wd.findElement(By.name("middlename")).sendKeys("Вованович");
-    wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
     wd.findElement(By.name("lastname")).sendKeys("Вованов");
@@ -45,8 +46,6 @@ public class ContactCreateTest {
     wd.findElement(By.name("byear")).click();
     wd.findElement(By.name("byear")).clear();
     wd.findElement(By.name("byear")).sendKeys("1987");
-    wd.findElement(By.name("new_group")).click();
-    wd.findElement(By.name("theform")).click();
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
     wd.findElement(By.name("company")).sendKeys("Снурфики лимитед");
@@ -55,13 +54,10 @@ public class ContactCreateTest {
     wd.findElement(By.name("mobile")).sendKeys("+79151591519");
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
     wd.findElement(By.linkText("home")).click();
-//    wd.findElement(By.id("popupInjectionKR")).click();
-//    wd.findElement(By.id("popupInjectionKR")).click();
-//    wd.findElement(By.xpath("//div[@id='top']/form/b")).click();
     wd.findElement(By.linkText("Logout")).click();
   }
 
-  @AfterMethod(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
